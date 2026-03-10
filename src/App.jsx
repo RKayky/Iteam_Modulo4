1603eb1edff9dd963c62cb709330bfb50ab6e272
RKayky <ryan.kayky10@gmail.com>
2026-03-10 19:47:50 -0400

    Primeiro projeto React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from './components/Footer'
+import './App.css'
+
+function App(){
+  return(
+    <div>
+      <h1>My Daily Habits</h1>
+      <p>Gerêncie seus hábitos de forma simples e visual.</p>
+      
+      <Footer/>
+    </div>
+  )
+}
+export default App
